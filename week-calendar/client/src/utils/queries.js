e4583665c94b878d26008ae9c990f4cf86ec827b
lwrnce <lwrnceyuen@gmail.com>
2021-07-08 16:48:25 -0700

    create queries

---
+++ week-calendar/client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import gql from 'graphql-tag';
+
+export const QUERY_USER = gql`
+{
+  user {
+    firstName
+    lastName
+    orders {
+      _id
+      purchaseDate
+      products {
+        _id
+        name
+        description
+        price
+        quantity
+        image
+      }
+    }
+  }
+}
+`;

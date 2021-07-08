779298d39af221becd6c28b2d8ca5d87ed93d0f9
lwrnce <lwrnceyuen@gmail.com>
2021-07-08 16:36:47 -0700

    create home page

---
+++ week-calendar/src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import CurrentDate from './components/CurrentDate';
+import { useState } from 'react';
+import TimeTable from './components/TimeTables';
+
+
+
+const Home = () => {
+  const [value, onChange, month] = useState(new Date());
+  return (
+      <div>
+          <Container maxWidth="xs">
+              <Grid container spacing={4} justify="center">
+                <Grid item>
+                  <CurrentDate/>
+                </Grid>
+              </Grid>
+                <Calendar
+                  onChance={onChange}
+                  value={value}
+                  view={month}
+                />
+          </Container>
+          <TimeTable/>
+        </div>
+  );
+}
+
+export default Home;

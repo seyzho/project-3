71b36adc8a7a850f8cc68618b7d7470824648d36
lwrnce <lwrnceyuen@gmail.com>
2021-07-08 16:39:49 -0700

    create navbar component

--- week-calendar/src/App.js
+++ week-calendar/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import { Button } from '@material-ui/core';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,7 +11,6 @@ import { useState } from 'react';
 import TimeTable from './components/TimeTables';
 
 function App() {
-  const [value, onChange, month] = useState(new Date());
   return (
     <React.Fragment>
       <CssBaseline/>
@@ -22,21 +20,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <main>
-      <div>
-          <Container maxWidth="xs">
-              <Grid container spacing={4} justify="center">
-                <Grid item>
-                  <CurrentDate/>
-                </Grid>
-              </Grid>
-                <Calendar
-                  onChance={onChange}
-                  value={value}
-                  view={month}
-                />
-          </Container>
-          <TimeTable/>
-        </div>
+      
       </main>
     </React.Fragment>
   );
